c5025d54d17b2daab03e0d12bc07df468bddf31e
George Donnelly <me@georgedonnelly.com>
2025-03-25 20:32:25 +0000

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"], // Scan TSX files in src
+  theme: {
+    extend: {}, // Add customizations here later if needed
+  },
+  plugins: [],
+};
